786362eb6dbb74dc0cc418cb97dfbaf086f9fad3
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-11-23 08:54:21 +0500

    MODELS: Create Host Model

---
+++ Sheenam.MVC/Models/Foundations/Hosts/Host.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Sheenam.MVC.Models.Foundations.Hosts
+{
+    public class Host
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$", ErrorMessage = "Email is invalid")]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
