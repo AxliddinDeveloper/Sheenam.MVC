08222303ff93611e868e52b35af5c60eb563bb1f
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-12-11 11:36:42 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Sheenam.MVC.Test.Unit/Foundations/Guests/GuestServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,54 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Moq;
+using System.Threading.Tasks;
+using System;
+using Xunit;
+using Sheenam.MVC.Models.Foundations.Guests.Exceptions;
+using Sheenam.MVC.Models.Foundations.Guests;
+using FluentAssertions;
+
+namespace Sheenam.MVC.Test.Unit.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidGuestId = Guid.Empty;
+            var invalidGuestException = new InvalidGuestExcpetion();
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.Id),
+                values: "Id is required");
+
+            var expectedGuestValidationException = new
+                GuestValidationException(invalidGuestException);
+
+            // when
+            ValueTask<Guest> retrieveGuestByIdTask =
+                this.guestService.RetrieveGuestByIdAsync(invalidGuestId);
+
+            GuestValidationException actualGuestValidationException =
+                await Assert.ThrowsAsync<GuestValidationException>(
+                    retrieveGuestByIdTask.AsTask);
+
+            // then
+            actualGuestValidationException.Should().BeEquivalentTo(expectedGuestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
