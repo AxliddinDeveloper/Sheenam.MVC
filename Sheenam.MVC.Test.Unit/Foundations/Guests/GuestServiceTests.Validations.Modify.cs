0bbd81f9c5c96084ab0586974055436191629d47
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-12-11 14:19:55 +0500

    ShouldThrowValidationExceptionOnModifyIfGuestIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.MVC.Test.Unit/Foundations/Guests/GuestServiceTests.Validations.Modify.cs
@@ -0,0 +1,49 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.MVC.Models.Foundations.Guests;
+using Sheenam.MVC.Models.Foundations.Guests.Exceptions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sheenam.MVC.Test.Unit.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfGuestIsNullAndLogItAsync()
+        {
+            // given
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestValidationException =
+                new GuestValidationException(nullGuestException);
+
+            // when
+            ValueTask<Guest> modifyGuestTask =
+                this.guestService.ModifyGuestAsync(nullGuest);
+
+            GuestValidationException actualGuestValidationException =
+                await Assert.ThrowsAsync<GuestValidationException>(
+                    modifyGuestTask.AsTask);
+
+            // then
+            actualGuestValidationException.Should().BeEquivalentTo(expectedGuestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateGuestAsync(It.IsAny<Guest>()), Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
