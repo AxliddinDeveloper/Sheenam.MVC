c6af05a5477b645266678fac811ac0063837bbf6
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2023-12-16 11:50:10 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

--- Sheenam.MVC/Services/Foundations/Hosts/HostService.cs
+++ Sheenam.MVC/Services/Foundations/Hosts/HostService.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Sheenam.MVC.Brokers.Loggings;
 using Sheenam.MVC.Brokers.Storages;
 using Sheenam.MVC.Models.Foundations.Hosts;
+using Sheenam.MVC.Models.Foundations.Hosts.Exceptions;
 
 namespace Sheenam.MVC.Services.Foundations.Hosts
 {
@@ -25,7 +26,22 @@ namespace Sheenam.MVC.Services.Foundations.Hosts
 
         public async ValueTask<Host> AddHostAsync(Host host)
         {
-            return await this.storagebroker.InsertHostAsync(host);
+            try
+            {
+                if (host == null)
+                    throw new NullHostException();
+
+                return await this.storagebroker.InsertHostAsync(host);
+            }
+            catch (NullHostException nullHostException)
+            {
+                var hostValidationException = 
+                    new HostValidationException(nullHostException);
+
+                this.loggingbroker.LogError(hostValidationException);
+
+                throw hostValidationException;
+            }
         }
 
         public IQueryable<Host> RetrieveAllHosts()
